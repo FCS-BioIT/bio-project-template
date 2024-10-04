dc96ddc97b4f3bdadcf512d4fdc99faa490fea66
Petr <petr.volkov@mshareservices.es>
2024-10-04 16:04:40 +0200

    Added _gitignore and a hook to rename to .gitignore

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -11,5 +11,19 @@ def remove_gitkeep_files():
                 os.remove(os.path.join(root, file))
 
 
+def rename_gitignore():
+    """Rename _gitignore to .gitignore."""
+    old_path = os.path.join(os.getcwd(), '_gitignore')
+    new_path = os.path.join(os.getcwd(), '.gitignore')
+
+    if os.path.exists(old_path):
+        os.rename(old_path, new_path)
+        print("renaming gitignore")
+
+    else:
+        raise FileNotFoundError("_gitignore file not found")
+
+
 if __name__ == "__main__":
     remove_gitkeep_files()
+    rename_gitignore()
