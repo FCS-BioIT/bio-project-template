1182f49623e67c4e153ea29d9430372e82648559
Petr <petr.volkov@mshareservices.es>
2024-10-07 11:16:10 +0200

    Moved snakemake parameteres to profile

--- {{ cookiecutter.project_slug }}/run.sh
+++ {{ cookiecutter.project_slug }}/run.sh
@@ -1,7 +1,7 @@
 WORKDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SNAKEDIR=$(realpath -m "$WORKDIR/src/Snakefile")
+CONFIGDIR=$(realpath -m "$WORKDIR/config/snakemake-profile")
 
 snakemake -d $WORKDIR \
 	  -s $SNAKEDIR \
-	  --executor slurm \
-	  --jobs 1
+	  --workflow-profile $CONFIGDIR
